e5587e82eb97b16c592b7c125fe5b8ef43788b9a
Victoria <victoria.sofianidou@gmail.com>
2020-09-16 15:32:16 +0200

    update

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 
-document.getElementById("search").addEventListener("click", async()=>{
+
+
+document.getElementById("search").addEventListener("click", ()=>{
 
     let city= document.getElementById("input").value;
     let apiKeyUrl =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=8fb9457f203009590ef435a9f37072b6`
 
-
-    await fetch(apiKeyUrl)
+    fetch(apiKeyUrl)
         .then(response => response.json())
         .then(data => console.log(data))
         .then(data => showData(data))
         .catch(err => console.error(err));
 
+    fetch(`https://api.unsplash.com/photos/random?query=${city}&client_id=So61IiiIyjV1LZs7oCUzjSKdKmKAYHt8O93pE88X9E0`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .then(data => showImage(data))
+        .then(err => console.error(err));
+
+
 })
+
+
+function showImage(city){
+
+    document.getElementById("cityImg").src = city.urls.regular
+
+}
+
 
 function showData(weather){
 
