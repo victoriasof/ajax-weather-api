e02aa1b64cdcaa13362842478fdc1fb9f23342a9
Victoria <victoria.sofianidou@gmail.com>
2020-09-16 11:49:53 +0200

    added async, await

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-document.getElementById("search").addEventListener("click", ()=>{
+document.getElementById("search").addEventListener("click", async()=>{
 
     let city= document.getElementById("input").value;
     let apiKeyUrl =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=8fb9457f203009590ef435a9f37072b6`
 
 
-    fetch(apiKeyUrl)
+    await fetch(apiKeyUrl)
         .then(response => response.json())
         .then(data => console.log(data))
         .then(data => showData(data))
