f08dd51b1ac39912f8c4a8a6f6b8604430f85719
Victoria <victoria.sofianidou@gmail.com>
2020-09-17 11:24:33 +0200

    made some little changes

--- script.js
+++ script.js
@@ -1,57 +1,62 @@
 
 
+let city= document.getElementById("input").value;
+//made city a global variable
 
 document.getElementById("search").addEventListener("click", ()=>{
 
     let mykey = config.MY_KEY;
     let key2 = config.KEY_2;
 
-    let city= document.getElementById("input").value;
-
     let apiKeyUrl =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=` + mykey
     let keyUnsplash = `https://api.unsplash.com/photos/random?query=${city}&client_id=` + key2
-    
+
     fetch(apiKeyUrl)
         .then(response => response.json())
         .then(data => showData(data))
         .catch(err => console.error(err));
 
+    /*
     fetch(keyUnsplash)
         .then(response => response.json())
         .then(data => showImage(data))
         .catch(err => console.error(err));
+     */
 
 })
 
+/*
 function showImage(image){
     document.getElementById("cityImg").src = image.urls.thumb
+    document.getElementById("cityName").innerHTML= city
 }
+ */
 
 function showData(forecast){
 
-    document.getElementById("date1").innerHTML = forecast.list[8].dt_txt
-    //document.getElementById("img1").src = forecast.list[8].weather.icon
-    document.getElementById("temp1").innerHTML = forecast.list[8].main.temp
-    document.getElementById("desc1").innerHTML = forecast.list[8].weather[0].description
+    document.getElementById("date1").innerHTML = forecast.list[7].dt_txt;
+    document.getElementById("img1").src = forecast.list[7].weather.icon;
+    document.getElementById("temp1").innerHTML = forecast.list[7].main.temp + " °C";
+    document.getElementById("desc1").innerHTML = forecast.list[7].weather[0].description;
 
-    document.getElementById("date2").innerHTML = forecast.list[16].dt_txt
-    //document.getElementById("img2").src = forecast.list[16].weather.icon
-    document.getElementById("temp2").innerHTML = forecast.list[16].main.temp
-    document.getElementById("desc2").innerHTML = forecast.list[16].weather[0].description
+    document.getElementById("date2").innerHTML = forecast.list[15].dt_txt;
+    //document.getElementById("img2").src = forecast.list[15].weather.icon;
+    document.getElementById("temp2").innerHTML = forecast.list[15].main.temp + " °C";
+    document.getElementById("desc2").innerHTML = forecast.list[15].weather[0].description;
 
-    document.getElementById("date3").innerHTML = forecast.list[24].dt_txt
-    //document.getElementById("img3").src = forecast.list[24].weather.icon
-    document.getElementById("temp3").innerHTML = forecast.list[24].main.temp
-    document.getElementById("desc3").innerHTML = forecast.list[24].weather[0].description
+    document.getElementById("date3").innerHTML = forecast.list[23].dt_txt;
+    //document.getElementById("img3").src = forecast.list[23].weather.icon;
+    document.getElementById("temp3").innerHTML = forecast.list[23].main.temp + " °C";
+    document.getElementById("desc3").innerHTML = forecast.list[23].weather[0].description;
 
-    document.getElementById("date4").innerHTML = forecast.list[32].dt_txt
-    //document.getElementById("img4").src = forecast.list[32].weather.icon
-    document.getElementById("temp4").innerHTML = forecast.list[32].main.temp
-    document.getElementById("desc4").innerHTML = forecast.list[32].weather[0].description
+    document.getElementById("date4").innerHTML = forecast.list[31].dt_txt;
+    //document.getElementById("img4").src = forecast.list[31].weather.icon;
+    document.getElementById("temp4").innerHTML = forecast.list[31].main.temp + " °C"
+    document.getElementById("desc4").innerHTML = forecast.list[31].weather[0].description;
 
-    document.getElementById("date5").innerHTML = forecast.list[39].dt_txt
-    //document.getElementById("img5").src = forecast.list[39].weather.icon
-    document.getElementById("temp5").innerHTML = forecast.list[39].main.temp
-    document.getElementById("desc5").innerHTML = forecast.list[39].weather[0].description
+    document.getElementById("date5").innerHTML = forecast.list[39].dt_txt;
+    //document.getElementById("img5").src = forecast.list[39].weather.icon;
+    document.getElementById("temp5").innerHTML = forecast.list[39].main.temp + " °C";
+    document.getElementById("desc5").innerHTML = forecast.list[39].weather[0].description;
 
 }
